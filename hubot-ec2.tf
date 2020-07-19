5d95b958be961a2450aa0314b702d1d668bb7eee
James Hale <james@jameshale.net>
2020-07-19 09:34:30 -0400

    Switch to Discord and use separate unit file

--- hubot-ec2.tf
+++ hubot-ec2.tf
@@ -34,11 +34,11 @@ apt-get install -y awscli python-pip jq npm nodejs
 pip install botocore --upgrade
 pip install boto3 --upgrade
 npm install -g yo generator-hubot
-echo HUBOT_SLACK_TOKEN=$(aws --region us-east-1 ssm get-parameter --name hubot-slack-token --with-decryption | jq -r '.Parameter.Value') | sudo tee -a /etc/environment
+echo HUBOT_DISCORD_TOKEN=$(aws --region us-east-1 ssm get-parameter --name hubot-discord-token --with-decryption | jq -r '.Parameter.Value') | sudo tee -a /etc/environment
 echo REDIS_URL=redis://${aws_elasticache_cluster.hubot-redis.cache_nodes[0].address}:6379/dogbot | sudo tee -a /etc/environment
-git clone https://github.com/jmhale/dogbot.git /home/ubuntu/dogbot
+git clone --single-branch --branch v2 https://github.com/jmhale/dogbot.git /home/ubuntu/dogbot
 chown -R ubuntu:ubuntu /home/ubuntu/dogbot
-echo W1VuaXRdCkRlc2NyaXB0aW9uPUh1Ym90ClJlcXVpcmVzPW5ldHdvcmsudGFyZ2V0CkFmdGVyPW5ldHdvcmsudGFyZ2V0CgpbU2VydmljZV0KVHlwZT1zaW1wbGUKV29ya2luZ0RpcmVjdG9yeT0vaG9tZS91YnVudHUvZG9nYm90ClVzZXI9dWJ1bnR1CgpSZXN0YXJ0PWFsd2F5cwpSZXN0YXJ0U2VjPTEwCgpFbnZpcm9ubWVudEZpbGU9L2V0Yy9lbnZpcm9ubWVudAoKRXhlY1N0YXJ0PS9ob21lL3VidW50dS9kb2dib3QvYmluL2h1Ym90IC0tYWRhcHRlciBzbGFjawoKW0luc3RhbGxdCldhbnRlZEJ5PW11bHRpLXVzZXIudGFyZ2V0Cg== | base64 --decode > /etc/systemd/system/hubot.service
+echo ${filebase64("${path.module}/hubot.service")} | base64 --decode > /etc/systemd/system/hubot.service
 systemctl daemon-reload
 systemctl enable hubot
 systemctl start hubot
